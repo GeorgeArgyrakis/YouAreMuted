ac67bcaa8ed34df2add756b62b75424ba7efa46f
George Argyrakis <g.argyrakis@pobuca.com>
2022-02-16 23:31:33 +0200

    Automatic versioning

---
+++ GlobalMicMute/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information
+[assembly: AssemblyTitle("YouAreMuted")]
+//[assembly: AssemblyDescription("Insert description here")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("Insert company here")]
+//[assembly: AssemblyProduct("Insert product here")]
+[assembly: AssemblyCopyright("George Argyrakis")]
+//[assembly: AssemblyTrademark("Insert trademark here")]
+//[assembly: AssemblyCulture("")]
+
+// Version informationr(
+[assembly: AssemblyVersion("0.9.4.18")]
+[assembly: AssemblyFileVersion("0.9.4.18")]
+[assembly: NeutralResourcesLanguageAttribute( "en-US" )]
+
